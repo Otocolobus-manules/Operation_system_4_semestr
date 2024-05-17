2634973268139460f2416ddaaece57aedb2629a8
Чушка подзаборная <125672053+Otocolobus-manules@users.noreply.github.com>
2024-05-17 21:41:59 +0300

    Lab_8

---
+++ lab8.c
@@ -0,0 +1,71 @@
+#include <windows.h>
+#include <process.h>
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+
+HANDLE hthread[6], hsm, hsm1;
+char buffer[12], buff[12];
+
+void GoToXY(const int X, const int Y) {
+    HANDLE OutputHandle;
+    CONSOLE_SCREEN_BUFFER_INFO ScreenBufInfo;
+    OutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(OutputHandle, &ScreenBufInfo);
+    ScreenBufInfo.dwCursorPosition.X = X;
+    ScreenBufInfo.dwCursorPosition.Y = Y;
+    SetConsoleCursorPosition(OutputHandle, ScreenBufInfo.dwCursorPosition);
+}
+
+DWORD WINAPI Writer(void* n) {
+    short i;
+    while (1) {
+        WaitForSingleObject(hsm, 3000);
+        if ((int)n == 3) {
+            strncpy(buff, "Novosibirsk", 12);
+        } else if ((int)n == 4) {
+            strncpy(buff, "Ekaterinburg", 12);
+        } else if ((int)n == 5) {
+            strncpy(buff, "Semipalatink", 12);
+        }
+        for (i = 0; i <= 5; i++) {
+            buffer[i] = buff[i];
+        }
+        Sleep(1000);
+        for (i = 6; i <= 11; i++) {
+            buffer[i] = buff[i];
+        }
+        ReleaseSemaphore(hsm1, 1, NULL);
+    }
+    return 0;
+}
+
+DWORD WINAPI Reader(void* n) {
+    short i = 0, j;
+    while (1) {
+        i++;
+        WaitForSingleObject(hsm1, INFINITE);
+        GoToXY(((int)n + 1) * 20, i);
+        printf("%s", buffer);
+        ReleaseSemaphore(hsm, 1, NULL);
+        Sleep(290);
+    }
+    return 0;
+}
+
+void main() {
+    int i;
+    system("cls");
+    hsm = CreateSemaphore(NULL, 0, 1, NULL);
+    hsm1 = CreateSemaphore(NULL, 0, 1, NULL);
+    for (i = 3; i <= 5; i++) {
+        hthread[i] = CreateThread(NULL, 4096, Writer, (void*)i, 0, NULL);
+    }
+    for (i = 0; i <= 2; i++) {
+        hthread[i] = CreateThread(NULL, 4096, Reader, (void*)i, 0, NULL);
+    }
+    ReleaseSemaphore(hsm, 1, NULL);
+    getchar();
+    for (i = 0; i <= 5; i++) CloseHandle(hthread[i]);
+    CloseHandle(hsm);
+}
